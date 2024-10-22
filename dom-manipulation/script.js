b2b25247af1b561e58abe06cd909c077147ac299
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-10-22 19:50:34 +0200

    update script.js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -25,8 +25,36 @@ function ShowRandomQuote() {
     newDiv.AddQuoteForm('title');
     newDiv.textContent = 'This is a random quote';
 }
-// 
+// select the "generate quote" button
+const btnQuotes = document.getElementById('new-quote');
+// console.log(btnQuotes)
 
+// select the "quote category display"
+const quoteDisplay = document.getElementById('quote');
+// console.log(quoteDisplay)
 
+// select the "text display"
+const textDisplay = document.getElementById('text');
+// console.log(textDisplay)
+
+// select the "Add quotes button"
+const addNewQuotes = document.getElementById('add-quotes-button');
+// console.log(addNewQuotes)
+
+// function to display the content of the array "quotes" after click
+// in the "generate quote button"
+ShowNewQuote.addEventListener('click', () => {
+    let random = text.category(text.random() *quotes.length);
+    // console.log(random);
+    quoteDisplay.innerText = quotes[random].quote
+    categoryDisplay.innerText = qoutes[random].category;
+})
+// addNewQuotes.addEventListener('click', function (){
+//  textInput = document.getElementById('addtext').value;
+//  // console.log(textInput);
+// quotesInput = document,getElementById('addquote').value;
+//  // console.log(quotesInput);
+// quotes.push(textInput)
+// })
 
 
