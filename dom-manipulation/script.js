35929da6ccc22d6aec8e4de7c9899b6049904b9e
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-10-22 16:17:42 +0200

    update script.js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,29 +1,32 @@
-// Accessing an element by ID
-const headerElement = document.getElementById('header');
-// Accessing elements by class name
-const paragraphs = document.getElementsByClassName('paragraph');
-// Accessing elements by tag name
-const images = document.getElementsByTagName('img');
-// Modifying the content of an element
-headerElement.innerHTML ='New Header Text';
-// Accessing a button element
-const myButton = document.getElementById('myButton');
-// Adding a click event listener
-myButton.addEventListener('click', fuction() {
-
+document.getElementById('container');
+<div id="container"></div>
+document.querySelector('.highlight');
+<p class="highlight">newQuote</p>
+// Retrieving Element Content with DOM
+const Content = document.querySelector(".Highlight")
+Content.textContent
+// USing array form
+const listItem = document.getElementById('myList').category;
+const itemListArray = Array.from(listItem);
+itemListArray.forEach(function(item) {
+    item.textContent = 'Array: ${item.textcontent}';
 });
-// Accessing aparagraph element
- const myParagraph = document.getElementById('myParagraph')
- // Accessing a button element
- const colorButton = document,getElementById('colorButton');
- // Add a click event listener to the button
- colorButton.addEventListener('click', function() {
-    // Changing the style of the pharagraph
-    myParagraph.style.color = 'blue';
- });
- // Creating a new paragraph element
- const newParagraph = document.createElement('p');
- // Setting the text content of new paragraph
- newParagraph.textContent = 'This is a new paragraph.';
- // Appending the new paragraph to the body of the document
- document.body.appendChild(newParagraph);
+
+
+
+
+
+
+
+
+
+function ShowRandomQuote() {
+    const  newDiv = document.createElement();
+    newDiv.AddQuoteForm('title');
+    newDiv.textContent = 'This is a random quote';
+}
+// 
+
+
+
+
