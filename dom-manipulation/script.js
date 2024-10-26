d3df793ccd4b8ad24af4d4700030f1bfe4ccc4d3
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-10-26 15:13:12 +0200

    update script.js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,3 +1,18 @@
+// Array of quotes
+const myQuotes = [ 
+];
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+const todayQuote = quotes[Math.floor(Math.random() * quotes.length)];
+quote.innerHTML = todayQuote.quote;
+Author.innerHTML = todaysQuote.author;
+// Random background
+const Image = ["0,jpeg", "1.jpeg"];
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+const bgImage = document.createElement("img");
+bgImage.src = 'img/${chosenImage}';
+document.body.appendChild(bgImage);
+// Function to create newQuotes
 document.getElementById('container');
 <div id="container"></div>
 document.querySelector('.highlight');
@@ -19,4 +34,15 @@ function getRandomQuote () {
     return getRandomQuote;
 }
 const buttonQuote = document.getElementById('new-quote');
-buttonQuote.addEventListener('click', generate);
+buttonQuote.addEventListener('click', generate); 
+// Function to add a new quote
+function addQuote() {
+    const newQuoteText = document.getAnimations('newQuoteText').value;
+    const newCategory = document.getElementById('newQuoteCategory'). value;
+}
+// Listene for events on element
+newQuoteButton.addEventListener('click', getQuote);
+// Fuction created
+function getQuote(){
+    console.log("quote button was cloicked");
+}
